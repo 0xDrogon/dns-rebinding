c54bbfb28733b8e77a6745768e2aa1f5886f6abd
Drogon <48291007+0xDrogon@users.noreply.github.com>
2022-10-17 22:56:13 +0100

    First commit
    Adds all files to the repo

---
+++ victim/app/static/js/main.js
@@ -0,0 +1,76 @@
+function bedroomToggle() {
+    if(document.getElementById("bedroom").checked === true){
+        $.get('/password', function(data) {
+            $.post('/lights?room=bedroom&value=true&otp='+data.otp, function(data) {
+                console.log("bedroom on")
+            })
+        })
+    } else {
+        $.get('/password', function(data) {
+            $.post('/lights?room=bedroom&value=false&otp='+data.otp, function(data) {
+                console.log("bedroom off")
+            })
+        })
+    }
+}
+function kitchenToggle() {
+    if(document.getElementById("kitchen").checked === true){
+        $.get('/password', function(data) {
+            $.post('/lights?room=kitchen&value=true&otp='+data.otp, function(data) {
+                console.log("kitchen on")
+            })
+        })
+    } else {
+        $.get('/password', function(data) {
+            $.post('/lights?room=kitchen&value=false&otp='+data.otp, function(data) {
+                console.log("kitchen off")
+            })
+        })
+    }
+}
+function livingroomToggle() {
+    if(document.getElementById("livingroom").checked === true){
+        $.get('/password', function(data) {
+            $.post('/lights?room=livingroom&value=true&otp='+data.otp, function(data) {
+                console.log("livingroom on")
+            })
+        })
+    } else {
+        $.get('/password', function(data) {
+            $.post('/lights?room=livingroom&value=false&otp='+data.otp, function(data) {
+                console.log("livingroom off")
+            })
+        })
+    }
+}
+function bathroomToggle() {
+    if(document.getElementById("bathroom").checked === true){
+        $.get('/password', function(data) {
+            $.post('/lights?room=bathroom&value=true&otp='+data.otp, function(data) {
+                console.log("bathroom on")
+            })
+        })
+    } else {
+        $.get('/password', function(data) {
+            $.post('/lights?room=bathroom&value=false&otp='+data.otp, function(data) {
+                console.log("bathroom off")
+            })
+        })
+    }
+}
+function hallwayToggle() {
+    if(document.getElementById("hallway").checked === true){
+        $.get('/password', function(data) {
+            $.post('/lights?room=hallway&value=true&otp='+data.otp, function(data) {
+                console.log("hallway on")
+            })
+        })
+    } else {
+        $.get('/password', function(data) {
+            $.post('/lights?room=hallway&value=false&otp='+data.otp, function(data) {
+                console.log("hallway off")
+            })
+        })
+    }
+}
+
---
+++ victim/app/victim.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, json
+import string, random
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+password = ''
+lights = {
+    "bedroom": False,
+    "kitchen": False,
+    "livingroom": False,
+    "bathroom": False,
+    "hallway": False
+}
+
+@app.route('/', methods=('GET',))
+def home():
+    return render_template('index.html')
+
+@app.route('/password', methods=('GET',))
+def getPassword():
+    global password
+    password = ''.join(random.choices(string.ascii_letters, k=10))
+    data = {
+        'otp': password
+    }
+    return app.response_class(
+        response=json.dumps(data),
+        mimetype='application/json; charset=utf-8'
+    )
+
+@app.route('/lights', methods=('GET',))
+def getLights():
+    global lights
+    logging.debug(lights)
+    return app.response_class(
+        response=json.dumps(lights),
+        mimetype='application/json; charset=utf-8'
+    )
+
+@app.route('/lights', methods=('POST',))
+def setLights():
+    global password, lights
+    otp = request.args.get('otp', None)
+    if otp != password or password == '':
+        return app.response_class(
+            response='wrong password',
+            status=401
+        )
+    room = request.args.get('room')
+    value = request.args.get('value')
+    if value == 'true':
+        lights[room] = True
+    elif value == 'false':
+        lights[room] = False
+    else:
+        return app.response_class(
+            response='not value',
+            status=400
+        )
+    password = ''
+    return app.response_class(
+        response=json.dumps(lights),
+        mimetype='application/json; charset=utf-8'
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
